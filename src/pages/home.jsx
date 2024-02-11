725e42dfc4157ff5ff3988ba3b710eedca33e99b
brian tran <btran@hamilton.edu>
2024-02-11 20:34:24 +0100

    home udpate

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -6,12 +6,26 @@ import Typography from "@mui/material/Typography";
 // import Button from "@mui/material/Button";
 // import Profile from "../components/profile.jsx";
 
+import ClearIcon from "@mui/icons-material/Clear";
+import CheckIcon from "@mui/icons-material/Check";
+import StarBorderIcon from "@mui/icons-material/StarBorder";
+
 const Home = () => {
   return (
     <Stack>
       <Typography variant="h1">Hamily</Typography>
 
-      <Typography>Vote and see your results!</Typography>
+      <Typography>
+        Voting <ClearIcon /> means you don't know them.
+      </Typography>
+      <Typography>
+        Voting <CheckIcon /> means you know them.
+      </Typography>
+      <Typography>
+        Voting <StarBorderIcon /> means you know them well.
+      </Typography>
+
+      <Typography>Check the rankings for interesting results!</Typography>
     </Stack>
   );
 };
