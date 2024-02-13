c9acb6f3c975d2117059269ea679246b79d30d05
brian tran <btran@hamilton.edu>
2024-02-13 23:36:36 +0100

    information given

--- src/components/loading.jsx
+++ src/components/loading.jsx
@@ -1,13 +1,14 @@
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
+import { isMobile } from "react-device-detect";
 
 const Loading = () => {
   return (
     <Stack>
-      <Typography variant="h1">Loading...</Typography>
-      <Typography>
-        Since Hamily uses free servers, loading takes a long time sometimes.
-        Give it a few mins! Thanks.
+      <Typography variant={`${isMobile ? "h1" : "h1"}`}>Loading...</Typography>
+      <Typography sx={{ fontSize: `${isMobile ? "16" : "20"}px` }}>
+        Since TheSocNet uses free servers (we have no money), loading takes a
+        long time sometimes. Give it a few mins! Thanks.
       </Typography>
     </Stack>
   );
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,31 +1,52 @@
-// import { useState, useEffect } from "react";
-// import { useOutletContext } from "react-router-dom";
-
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
-// import Button from "@mui/material/Button";
-// import Profile from "../components/profile.jsx";
-
 import ClearIcon from "@mui/icons-material/Clear";
 import CheckIcon from "@mui/icons-material/Check";
 import StarBorderIcon from "@mui/icons-material/StarBorder";
+import { isMobile } from "react-device-detect";
 
 const Home = () => {
   return (
     <Stack>
-      <Typography variant="h1">TheSocNet</Typography>
-      <Typography>You must login with your school email!</Typography>
-      <Typography>
-        Voting <ClearIcon /> means you don't know them.
+      <Typography variant={`${isMobile ? "h1" : "h1"}`}>Welcome!</Typography>
+      <Typography sx={{ fontSize: `${isMobile ? "16" : "20"}px` }}>
+        What is TheSocNet?
+        <br />
+        A poorly made website that aims to visualize the relationships between
+        students at Hamilton College.
+        <br />
+        <br />
+        How does it work?
+        <br />
+        First sign in with your school email. This way, every vote is from a
+        registered student and not some rando. Then vote!
+        <br />
+        Voting <ClearIcon /> means you don't know them at all.
+        <br />
+        Voting <StarBorderIcon /> means you know them well, like your friends.
+        <br />
+        Voting <CheckIcon /> means you know them, but not that well. Like
+        someone you worked with once or someone you met at a party. This is
+        super vague and up to you!
+        <br />
+        <br />
+        Who is on the rankings?
+        <br />
+        People that are voted the most well known or known by others. Keep in
+        mind, this doesn't necessarily imply the person is bad or good. We don't
+        encourage bullying.
+        <br />
+        <br />
+        Why is it so poorly made?
+        <br />
+        I'm an amateur with very little experience and I made this during my
+        sparetime. Hopefully, everything should be intuitive and easy to use.
+        <br />
+        <br />
+        Where do I send my complaints or concerns or compliments?
+        <br />
+        To your momma's house ;) or Yodel, either works. Now peace! :)
       </Typography>
-      <Typography>
-        Voting <CheckIcon /> means you know them.
-      </Typography>
-      <Typography>
-        Voting <StarBorderIcon /> means you know them well.
-      </Typography>
-
-      <Typography>Check the rankings for interesting results!</Typography>
     </Stack>
   );
 };
